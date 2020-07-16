6e3a3bcf910ddc11606d511294b82722ad943bad
German Espinosa <germanespinosa@gmail.com>
2020-07-16 17:50:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-sudo apt install libesd0-dev -y
-sudo apt install libwebp-dev libtiff-dev -y
-sudo apt install libvorbis-dev libflac-dev libfluidsynth-dev libmodplug-dev -y
-sudo apt install libsdl2-image-dev -y
-sudo apt install libsdl2-mixer-dev -y
-sudo apt install libsdl2-ttf-dev -y
+apt install libesd0-dev -y
+apt install libwebp-dev libtiff-dev -y
+apt install libvorbis-dev libflac-dev libfluidsynth-dev libmodplug-dev -y
+apt install libsdl2-image-dev -y
+apt install libsdl2-mixer-dev -y
+apt install libsdl2-ttf-dev -y
 
 rm simulation/ -r --force > /dev/null
 mkdir simulation
 cd simulation 
 
-git clone https://github.com/germanespinosa/utf8-cpp
-git clone https://github.com/germanespinosa/catch
-git clone https://github.com/germanespinosa/ge211
-git clone https://github.com/germanespinosa/gcomm
-git clone https://github.com/germanespinosa/cellworld
-git clone https://github.com/germanespinosa/hexaworld
+git clone git://germanespinosa/utf8-cpp
+git clone git://germanespinosa/catch
+git clone git://germanespinosa/ge211
+git clone git://germanespinosa/gcomm
+git clone git://germanespinosa/cellworld
+git clone git://germanespinosa/hexaworld
 
 wget https://raw.githubusercontent.com/germanespinosa/simulation/master/buildall.sh
 chmod +777 buildall.sh
