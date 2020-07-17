694501ce48b1da1897614915ed4948a4a7bc371e
German Espinosa <germanespinosa@gmail.com>
2020-07-16 19:30:05 -0500

    Update buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -6,34 +6,34 @@ mkdir build/utf8-cpp -p
 cd build/utf8-cpp
 cmake ../../utf8-cpp
 make
-sudo make install
+make install
 cd - > /dev/null
 
 mkdir build/catch -p
 cd build/catch
 cmake ../../catch
 make
-sudo make install
+make install
 cd -
 mkdir build/ge211 -p
 cd build/ge211
 cmake ../../ge211
 make
-sudo make install
+make install
 cd - > /dev/null
 
 mkdir build/gcomm -p
 cd build/gcomm
 cmake ../../gcomm
 make
-sudo make install
+make install
 cd - > /dev/null
 
 mkdir build/cellworld -p
 cd build/cellworld
 cmake ../../cellworld
 make
-sudo make install
+make install
 cd - > /dev/null
 
 mkdir build/hexaworld -p
