0614c175942c7f92075668361ad42809a1ed45ac
German Espinosa <germanespinosa@gmail.com>
2020-07-16 19:46:25 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ git clone git://github.com/germanespinosa/ge211.git
 git clone git://github.com/germanespinosa/cellworld.git
 git clone git://github.com/germanespinosa/hexaworld.git
 
-wget https://raw.githubusercontent.com/germanespinosa/simulation/master/buildall.sh
+wget https://raw.githubusercontent.com/germanespinosa/simulation/master/buildall.sh -q
 chmod +777 buildall.sh
 
 ./buildall.sh
