98bf42cef3666c0d093adf208f024780ee5f1529
German Espinosa <germanespinosa@gmail.com>
2020-07-18 21:17:47 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,7 @@ rm simulation/ -r --force > /dev/null
 mkdir simulation
 cd simulation 
 
-git clone git://github.com/germanespinosa/utf8-cpp.git
 git clone git://github.com/germanespinosa/catch.git
-git clone git://github.com/germanespinosa/ge211.git
 git clone git://github.com/germanespinosa/cellworld.git
 git clone git://github.com/germanespinosa/hexaworld.git
 
